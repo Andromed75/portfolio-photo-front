589cb23a1557173f310029f29c96a6ff73ffaf1b
Arthur-Emile DE DONNO <ae.de-donno@groupeonepoint.com>
2025-12-27 17:22:16 +0100

    wip

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  text = 'Arthur-Émile de DONNO';
+  letters: string[] = [];
+
+  ngOnInit(): void {
+  this.letters = this.text.split('').map(char =>
+    char === ' ' ? '\u00A0' : char
+  );  }
 
 }
